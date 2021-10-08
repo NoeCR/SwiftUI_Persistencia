bbccfd412d770e80df852658a0ae789bb11abdd6
Jose Garcia <jlgarciasande@gmail.com>
2021-10-08 09:56:37 +0200

    Initial Commit

---
+++ GasolinerasXcode12/GasolinerasXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  GasolinerasXcode12App.swift
+//  GasolinerasXcode12
+//
+//  Created by Jose Garcia on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GasolinerasXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
